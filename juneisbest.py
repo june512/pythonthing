cae61d3ea80c5c997088babd5ab96942561c604d
june512 <junehong@June-Hongs-MacBook.local>
2014-07-15 19:52:11 -0400

    june is best//

---
+++ juneisbest.py
@@ -0,0 +1,18 @@
+print ("Take this simple test to find out your IQ")
+print ("jk")
+print ("take this simple test \n")
+print ("Who is the best person alive?")
+userChoice = input("Your Choice: ")
+if userChoice == "June":
+    print ("correct")
+elif userChoice == "Jay":
+    print ("half credit")
+else:
+    print ("incorrect")
+print ("Who was our first president?")
+userChoice = input("Your Choice:")
+if userChoice == "George Washington":
+    print ("correct, good job")
+else:
+    print ("how do you not know this honestly")
+    
